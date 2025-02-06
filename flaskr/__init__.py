869250334f1a93295364de3ebdb4768fd0657c9a
clinter77 <clintchris7777@laposte.net>
2025-02-06 14:35:11 +0100

    commit du 06 février 2025 à 14h35 - ajout de la page 404

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -55,6 +55,10 @@ def create_app(test_config=None):
     def index():
         return render_template('index.html')
     
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('page_not_found.html'), 404
+    
     
     # fonctionnel avant
     # @app.route('/accueil')
