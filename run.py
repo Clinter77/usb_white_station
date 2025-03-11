19a2589ab1e5c20511ae580c98341619fa6c45a6
clinter77 <clintchris7777@laposte.net>
2025-03-11 15:33:27 +0100

    commit du 11/03/2025 à 15h33

--- run.py
+++ run.py
@@ -7,4 +7,5 @@ if __name__ == "__main__":
     # en local
     # app.run(host="0.0.0.0", port=5000, debug=True)
     # sur le Rapsberry PI
-    app.run(host="10.10.2.89", port=5000) 
+    # app.run(host="10.10.2.89", port=5000)
+    app.run(host="0.0.0.0", port=5000) 
