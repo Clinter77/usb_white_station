2adeb0059aab2e1effced4eb22a9684790530a8c
clinter77 <clintchris7777@laposte.net>
2025-03-11 14:37:40 +0100

    commit du 11/03/2025 à 14h37 - rajout pour qu'il puisse tourner sur l'IP de notre Rapsberry PI

--- run.py
+++ run.py
@@ -4,4 +4,7 @@ from flaskr import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True) 
+    # en local
+    # app.run(host="0.0.0.0", port=5000, debug=True)
+    # sur le Rapsberry PI
+    app.run(host="10.10.2.89", port=5000) 
